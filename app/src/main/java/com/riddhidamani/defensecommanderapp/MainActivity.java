3d37e44d10c6d4eeb53b105286b2a90f6fbc8816
Riddhi Damani <riddhidamani90@gmail.com>
2022-03-07 19:43:41 -0600

    minor update

--- app/src/main/java/com/riddhidamani/defensecommanderapp/MainActivity.java
+++ app/src/main/java/com/riddhidamani/defensecommanderapp/MainActivity.java
@@ -2,13 +2,47 @@ package com.riddhidamani.defensecommanderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+    private int screenHeight;
+    private int screenWidth;
+    private float leftScreenPart, midScreenPart, rightScreenPart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupFullScreen();
+        getScreenDimensions();
     }
+    private void setupFullScreen() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    private void getScreenDimensions() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenHeight = displayMetrics.heightPixels;
+        screenWidth = displayMetrics.widthPixels;
+        leftScreenPart = (float) (screenWidth * 0.33);
+        midScreenPart = (float) (screenWidth * 0.5);
+        rightScreenPart = (float) (screenWidth * 0.66);
+    }
+
 }
